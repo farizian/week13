24409ede21cb364df8ac06d38f38eb4c3d6bc6f9
farizian <farizirfan8@gmail.com>
2021-08-22 02:49:46 +0700

    update 21aug21

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const parser = require('body-parser');
 const userrouter = require('./src/routers/userrouter');
 const prdrouter = require('./src/routers/productrouter');
 const catrouter = require('./src/routers/categoryrouter');
 
 const app = express();
+app.use(cors());
 app.use(parser.json());
 app.use(userrouter);
 app.use(prdrouter);
